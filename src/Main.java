c5af82d90b06d04141d92dbfa0e484ec40282b48
Vira Zapotochna <Zapotochnav@gmail.com>
2023-02-07 10:58:21 +0200

    homework - 4 with fix

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
-import com.example.homework.homeworkFour.Employee;
-import com.example.homework.homeworkFour.HomeworkFour;
-import com.example.homework.homeworkFour.StudentTest;
 import com.example.homework.homeworkThree.HomeworkThree;
 import com.example.homework.homeworkTwo.HomeworkTwo;
 
 public class Main {
-
     public static void main(String[] args) {
-//        HomeworkTwo homeworkTwo = new HomeworkTwo();
-//        homeworkTwo.execution();
-//        HomeworkThree homeworkThree = new HomeworkThree();
-//        homeworkThree.execution();
-        HomeworkFour homeworkFour = new HomeworkFour();
-        homeworkFour.execution();
+        HomeworkTwo homeworkTwo = new HomeworkTwo();
+        homeworkTwo.execution();
+        HomeworkThree homeworkThree = new HomeworkThree();
+        homeworkThree.execution();
     }
-
 }
--- src/com/example/homework/homeworkFour/Workers.java
+++ src/com/example/homework/homeworkFour/Workers.java
@@ -1,23 +1,16 @@
 package com.example.homework.homeworkFour;
 
 public class Workers {
-    Employee employee1;
-    Employee employee2;
-
-    public void setParams() {
-        this.employee1 = new Employee(3000, "Green", 1);
-        this.employee2 = new Employee(3500, "Black", 2);
-        System.out.println(this.employee1.id + " " + this.employee1.surname);
-        System.out.println(this.employee2.id + " " + this.employee2.surname);
-
-        this.employee1.showId();
-        this.employee1.showSalary();
-        this.employee1.showSurname();
-
-        this.employee2.showId();
-        this.employee2.showSalary();
-        this.employee2.showSurname();
-
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(3000, "Green", 1);
+        Employee employee2 = new Employee(3500, "Black", 2);
+        System.out.println(employee1.id + " " + employee1.surname);
+        System.out.println(employee2.id + " " + employee2.surname);
+        employee1.showId();
+        employee1.showSalary();
+        employee1.showSurname();
+        employee2.showId();
+        employee2.showSalary();
+        employee2.showSurname();
     }
-
 }
